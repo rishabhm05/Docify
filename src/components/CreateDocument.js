c3eb2df3fb9ef4cd6f37d4b1c566d671ab0f3674
DAI\rishabh.mehta <rishabh.mehta@williamoneilindia.com>
2023-10-01 19:43:33 +0530

    Responsive

--- src/components/CreateDocument.js
+++ src/components/CreateDocument.js
@@ -4,7 +4,7 @@ const CreateDocument = ({toggleModal,settoggleModal}) => {
 
   return (
     <div className='bg-[whitesmoke] my-6 '>
-    <div className='max-w-[1200px] mx-auto  py-6'>
+    <div className='md:max-w-[1200px] md:mx-auto mx-2  py-6'>
      <p className='text-[gray]'>Start a new Document</p>
      <div className='relative w-40 h-52 cursor-pointer' onClick={()=>settoggleModal(!toggleModal)}>
      <Image src={"https://ssl.gstatic.com/docs/templates/thumbnails/docs-blank-googlecolors.png"} layout="fill"/> 
